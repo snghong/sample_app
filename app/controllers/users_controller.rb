b685fe92b3216b2a0c63945915f78e84098f0e20
snghong <sng_hong@outlook.com>
2021-05-01 23:50:53 +0800

    finished microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     
   end
   def edit
@@ -44,15 +45,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  # Confirms a logged-in user.
-  def logged_in_user
-    
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+  
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
